1b16b1aed533dfe213d6c55508796754ec4940ac
Deepak Dhakal <157896641+Deepak19-tech@users.noreply.github.com>
2026-08-25 07:44:37 +0545

    Improve ledger templates and authentication forms

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from .models import Budget, Category, Expense, Income
 
@@ -9,10 +9,25 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "first_name", "email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
     def save(self, commit=True):
         user = super().save(commit=False); user.email = self.cleaned_data["email"]; user.first_name = self.cleaned_data["first_name"]
         if commit: user.save()
         return user
+
+
+class LoginForm(AuthenticationForm):
+    """Apply the shared Bootstrap field styling to Django's login form."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
 
 class StyledForm(forms.ModelForm):
     def __init__(self, *args, user=None, **kwargs):
